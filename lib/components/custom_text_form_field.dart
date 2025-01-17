72a434e0bacc3de216790a3f4da443dba025de3a
Moheb Boules Guirges <mohebboules@gmail.com>
2025-01-17 20:47:33 +0200

    Adding obsecure text button for passwords

--- lib/components/custom_text_form_field.dart
+++ lib/components/custom_text_form_field.dart
@@ -1,28 +1,56 @@
 import 'package:flutter/material.dart';
 
-class CustomTextFormField extends StatelessWidget {
+class CustomTextFormField extends StatefulWidget {
   const CustomTextFormField(
       {super.key,
       required this.hintText,
       required this.onChanged,
-      this.obsecureText});
+      this.obsecureText = false});
 
   final String hintText;
   final Function(String)? onChanged;
-  final bool? obsecureText;
+  final bool obsecureText;
+
+  @override
+  State<CustomTextFormField> createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  late bool obsecureText;
+  @override
+  void initState() {
+    super.initState();
+    obsecureText = widget.obsecureText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      obscureText: obsecureText ?? false,
+      obscureText: obsecureText,
       validator: (data) {
         if (data == null || data.isEmpty) {
           return 'Field is required';
         }
         return null;
       },
-      onChanged: onChanged,
+      onChanged: widget.onChanged,
       decoration: InputDecoration(
-        hintText: hintText,
+        suffixIcon: widget.obsecureText == false
+            ? null
+            : IconButton(
+                onPressed: () {
+                  setState(() {
+                    obsecureText = !obsecureText;
+                  });
+                },
+                icon: Icon(
+                  obsecureText
+                      ? Icons.visibility_off_rounded
+                      : Icons.visibility_rounded,
+                  color: Colors.white,
+                ),
+              ),
+        hintText: widget.hintText,
         hintStyle: const TextStyle(color: Colors.white),
         enabledBorder: const OutlineInputBorder(
             borderSide: BorderSide(color: Colors.white)),
